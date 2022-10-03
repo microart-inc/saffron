07549cb6697b8ed104afafb4a794e7660a35dfa8
microart-inc <nasrj3@gmail.com>
2022-10-02 17:12:46 -0700

    sample

--- v2.js
+++ v2.js
@@ -4,10 +4,12 @@ import Container from './saffron2/container/container';
 import Text from './saffron2/text/text';
 import Heading from './saffron2/heading/heading';
 import Card from './saffron2/card/card';
+import Flex from './saffron2/flex/flex';
 
 export {
     Container,
     Text,
     Heading,
-    Card
+    Card,
+    Flex
 };
