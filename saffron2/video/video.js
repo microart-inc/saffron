6f90a49e1a476280c38551f13638b12f6e198c2b
microart-inc <nasrj3@gmail.com>
2022-10-25 22:05:43 -0700

    add video controls

--- saffron2/video/video.js
+++ saffron2/video/video.js
@@ -1,8 +1,8 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { istyleParser } from "../i";
 import styles from './video.module.css';
 import c from '../../CustomClass';
-import { IoPlay, IoVolumeHigh } from "react-icons/io5";
+import { IoPause, IoPlay, IoVolumeHigh } from "react-icons/io5";
 
 export default function Video(_props) {
     let props = { ..._props };
@@ -44,10 +44,67 @@ export default function Video(_props) {
     }
 
     const videoRef = useRef(null);
+    const progressBarRef = useRef(null);
+    const progressTrackRef = useRef(null);
     const [playing, setPlaying] = React.useState(false);
     const [showControls, setShowControls] = React.useState(false);
 
-    console.log(props);
+    const updatePlayerHeadPosition = () => {
+        const video = videoRef.current;
+        const progressBar = progressBarRef.current;
+        const percentage = (video.currentTime / video.duration) * 100;
+        progressBar.style.width = `${percentage}%`;
+        if (!video.ended) {
+            requestAnimationFrame(updatePlayerHeadPosition);
+        }
+    }
+
+    const updatePlayerStatePlay = () => {
+        setPlaying(true);
+        requestAnimationFrame(updatePlayerHeadPosition);
+    }
+
+    const updatePlayerStatePause = () => {
+        setPlaying(false);
+        cancelAnimationFrame(updatePlayerHeadPosition);
+    }
+
+    const updatePlayerStateEnd = () => {
+        setPlaying(false);
+        cancelAnimationFrame(updatePlayerHeadPosition);
+    }
+
+    const setPlayerStatePlay = () => {
+        videoRef.current.play();
+    }
+
+    const setPlayerStatePause = () => {
+        videoRef.current.pause();
+    }
+
+    const setProgress = (e) => {
+        if (!window.__pointerDown) return;
+        const video = videoRef.current;
+        const progressBar = progressTrackRef.current;
+        const percentage = (e.clientX - progressBar.getBoundingClientRect().left) / progressBar.getBoundingClientRect().width;
+        video.currentTime = percentage * video.duration;
+    }
+
+    useEffect(() => {
+        const setPointerDown = () => {
+            window.__pointerDown = true;
+        }
+        const setPointerUp = () => {
+            window.__pointerDown = false;
+        }
+        window.addEventListener('mousedown', setPointerDown);
+        window.addEventListener('mouseup', setPointerUp);
+
+        return () => {
+            window.removeEventListener('mousedown', setPointerDown);
+            window.removeEventListener('mouseup', setPointerUp);
+        }
+    }, []);
 
     return (
         <div className={c(styles.wrapper, parentClasses)} style={parentStyle}>
@@ -55,14 +112,27 @@ export default function Video(_props) {
                 {...props}
                 ref={videoRef}
                 className={c(styles.video, videoClasses)}
+                onPlay={updatePlayerStatePlay}
+                onPause={updatePlayerStatePause}
+                onEnded={updatePlayerStatePause}
             />
             <div className={styles.controls}>
                 <div></div>
                 <div className={styles.controlsBottom}>
-                    <div className={styles.progressWrapper}>
-                        <div className={styles.progressTrack}></div>
+                    <div className={styles.progressWrapper}
+                        onMouseDown={(e) => {
+                            window.__pointerDown = true;
+                            setProgress(e);
+                        }}
+                        onMouseMove={setProgress}
+                    >
+                        <div className={styles.progressTrack}
+                            ref={progressTrackRef}
+                        ></div>
                         <div className={styles.progressBuffered}></div>
-                        <div className={styles.progressBar} style={{ width: "50%" }}>
+                        <div className={styles.progressBar}
+                            ref={progressBarRef}
+                        >
                             <div className={styles.progress}></div>
                             <div className={styles.progressThumbWrapper}>
                                 <div className={styles.progressThumb}></div>
@@ -70,7 +140,15 @@ export default function Video(_props) {
                         </div>
                     </div>
                     <div className={styles.controlsBottomButtons}>
-                        <IoPlay />
+                        {playing ? (
+                            <IoPause
+                                onClick={setPlayerStatePause}
+                            />
+                        ) : (
+                            <IoPlay
+                                onClick={setPlayerStatePlay}
+                            />
+                        )}
                         <div className={styles.volume}>
                             <IoVolumeHigh />
                             <div className={styles.volumeSlider}>
