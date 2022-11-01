bbe244fd1142d38d4de36a5a221905abc795a67b
microart-inc <nasrj3@gmail.com>
2022-10-31 19:29:20 -0700

    added src list and mute

--- saffron2/video/video.js
+++ saffron2/video/video.js
@@ -2,11 +2,12 @@ import React, { useEffect, useRef } from "react";
 import { istyleParser } from "../i";
 import styles from './video.module.css';
 import c from '../../CustomClass';
-import { IoPause, IoPlay, IoVolumeHigh } from "react-icons/io5";
+import { IoPause, IoPlay, IoVolumeHigh, IoVolumeMute, IoWarning } from "react-icons/io5";
 
 export default function Video(_props) {
     let props = { ..._props };
 
+    //#region video seeking, volume, and other basic controls
     let parentStyle = {};
     let parentClasses = [];
 
@@ -50,8 +51,10 @@ export default function Video(_props) {
     const [playing, setPlaying] = React.useState(false);
     const [showControls, setShowControls] = React.useState(false);
     const [volume, setStateVolume] = React.useState(1);
+    const [muted, setMuted] = React.useState(false);
 
     const updatePlayerHeadPosition = () => {
+        if (!videoRef.current) return;
         const video = videoRef.current;
         const progressBar = progressBarRef.current;
         const percentage = (video.currentTime / video.duration) * 100;
@@ -88,6 +91,11 @@ export default function Video(_props) {
         videoRef.current.pause();
     }
 
+    const setPlayerStateMute = (isMuted = true) => {
+        videoRef.current.muted = isMuted;
+        setMuted(isMuted);
+    }
+
     const setProgress = (e) => {
         if (!window.__pointerDown) return;
         const video = videoRef.current;
@@ -104,6 +112,7 @@ export default function Video(_props) {
         percentage = Math.min(1, Math.max(0, percentage));
         if (percentage > 0.95) percentage = 1;
         video.volume = percentage;
+        setPlayerStateMute(false);
     }
 
     useEffect(() => {
@@ -122,78 +131,119 @@ export default function Video(_props) {
         }
     }, []);
 
+    //#endregion
+
+    const [error, setError] = React.useState(false);
+
+    let srcList;
+
+    if (props.src) {
+        srcList = Array.isArray(props.src) ? props.src : [props.src];
+        delete props.src;
+    } else if (!error) {
+        setError(true);
+    }
+
+    const [srcIndex, setSrcIndex] = React.useState(0);
+    const [src, setSrc] = React.useState(srcList?.[srcIndex]);
+
     return (
         <div className={c(styles.wrapper, parentClasses)} style={parentStyle}>
-            <video
-                {...props}
-                ref={videoRef}
-                className={c(styles.video, videoClasses)}
-                onPlay={updatePlayerStatePlay}
-                onPause={updatePlayerStatePause}
-                onEnded={updatePlayerStatePause}
-                onVolumeChange={updatePlayerVolume}
-            />
-            <div className={styles.controls}>
-                <div></div>
-                <div className={styles.controlsBottom}>
-                    <div className={styles.progressWrapper}
-                        onMouseDown={(e) => {
-                            window.__pointerDown = true;
-                            setProgress(e);
-                        }}
-                        onMouseMove={setProgress}
-                    >
-                        <div className={styles.progressTrack}
-                            ref={progressTrackRef}
-                        ></div>
-                        <div className={styles.progressBuffered}></div>
-                        <div className={styles.progressBar}
-                            ref={progressBarRef}
-                        >
-                            <div className={styles.progress}></div>
-                            <div className={styles.progressThumbWrapper}>
-                                <div className={styles.progressThumb}></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.controlsBottomButtons}>
-                        {playing ? (
-                            <IoPause
-                                onClick={setPlayerStatePause}
-                            />
-                        ) : (
-                            <IoPlay
-                                onClick={setPlayerStatePlay}
-                            />
-                        )}
-                        <div className={styles.volume}>
-                            <IoVolumeHigh />
-                            <div className={styles.volumeSlider}>
-                                <div className={styles.volumeSliderTrack}
-                                    ref={volumeRef}
-                                    onMouseDown={(e) => {
-                                        console.log(e);
-                                        window.__pointerDown = true;
-                                        setVolume(e);
-                                    }}
-                                    onMouseMove={setVolume}
-                                    onDragStart={(e) => e.preventDefault()}
-                                ></div>
-                                <div className={styles.volumeSliderBar}
-                                    style={{ width: volume * 100 + "%" }}
-                                    onMouseDown={(e) => {
-                                        console.log(e);
-                                        window.__pointerDown = true;
-                                        setVolume(e);
-                                    }}
-                                    onMouseMove={setVolume}
-                                    onDragStart={(e) => e.preventDefault()}
-                                ></div>
-                            </div>
-                        </div>
+            {error ? (
+                <div className={styles.error}>
+                    <IoWarning size="2.5em" />
+                    <div className={styles.errorInfo}>
+                        <span>Error playing video</span>
+                        <span>S-41</span>
                     </div>
                 </div>
-            </div>
-        </div>
+            ) : (
+                <>
+                    <video
+                        {...props}
+                        ref={videoRef}
+                        className={c(styles.video, videoClasses)}
+                        onPlay={updatePlayerStatePlay}
+                        onPause={updatePlayerStatePause}
+                        onEnded={updatePlayerStateEnd}
+                        onVolumeChange={updatePlayerVolume}
+                        src={src}
+                    />
+                    <div className={styles.controls}>
+                        <div></div>
+                        <div className={styles.controlsBottom}>
+                            <div className={styles.progressWrapper}
+                                onMouseDown={(e) => {
+                                    window.__pointerDown = true;
+                                    setProgress(e);
+                                }}
+                                onMouseMove={setProgress}
+                            >
+                                <div className={styles.progressTrack}
+                                    ref={progressTrackRef}
+                                ></div>
+                                <div className={styles.progressBuffered}></div>
+                                <div className={styles.progressBar}
+                                    ref={progressBarRef}
+                                >
+                                    <div className={styles.progress}></div>
+                                    <div className={styles.progressThumbWrapper}>
+                                        <div className={styles.progressThumb}></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.controlsBottomButtons}>
+                                {playing ? (
+                                    <IoPause
+                                        onClick={setPlayerStatePause}
+                                    />
+                                ) : (
+                                    <IoPlay
+                                        onClick={setPlayerStatePlay}
+                                    />
+                                )}
+                                <div className={styles.volume}>
+                                    {muted ? (
+                                        <IoVolumeMute
+                                            className={styles.volumeIcon}
+                                            onClick={() => {
+                                                setPlayerStateMute(false);
+                                            }}
+                                        />
+                                    ) : (
+                                        <IoVolumeHigh
+                                            className={styles.volumeIcon}
+                                            onClick={() => {
+                                                setPlayerStateMute(true);
+                                            }}
+                                        />
+                                    )}
+                                    <div className={styles.volumeSlider}>
+                                        <div className={styles.volumeSliderTrack}
+                                            ref={volumeRef}
+                                            onMouseDown={(e) => {
+                                                window.__pointerDown = true;
+                                                setVolume(e);
+                                            }}
+                                            onMouseMove={setVolume}
+                                            onDragStart={(e) => e.preventDefault()}
+                                        ></div>
+                                        <div className={styles.volumeSliderBar}
+                                            style={{ width: muted ? 0 : (volume * 100 + "%") }}
+                                            onMouseDown={(e) => {
+                                                window.__pointerDown = true;
+                                                setVolume(e);
+                                            }}
+                                            onMouseMove={setVolume}
+                                            onDragStart={(e) => e.preventDefault()}
+                                        ></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )}
+        </div >
     );
 }
